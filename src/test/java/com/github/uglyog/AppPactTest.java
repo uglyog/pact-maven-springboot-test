463df583b2fabdbe9dc11a9a225e616608de520c
Ronald Holshausen <uglyog@gmail.com>
2019-10-12 11:49:44 +1100

    chore: add junit pact test

---
+++ src/test/java/com/github/uglyog/AppPactTest.java
@@ -0,0 +1,26 @@
+package com.github.uglyog;
+
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.SpringRestPactRunner;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(SpringRestPactRunner.class)
+@Provider("spring-boot-maven-provider")
+//@PactBroker(scheme = "https", host = "${pactBrokerHost}", port = "443",
+//  authentication = @PactBrokerAuth(username = "${pactBrokerUser}", password = "${pactBrokerPassword}"))
+@PactFolder("pacts")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AppPactTest
+{
+    @TestTarget
+    @SuppressWarnings(value = "VisibilityModifier")
+    public final Target target = new HttpTarget(8080);
+}
